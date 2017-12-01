ec8634345d1a013b51d7ab639e38433d6f22cc4f
John Grosh <john.a.grosh@gmail.com>
2017-12-01 00:38:46 -0500

    added emote and twemoji support

--- js/custom.js
+++ js/custom.js
@@ -13,9 +13,15 @@ $(document).ready(function() {
 	var url = "https://cdn.discordapp.com/attachments/"+loc+".txt";
 	if(loc)
 		$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=?', function(data){
+			// filter html
 			var text = data.contents.split('<').join('&lt').split('>').join('&gt').split('\n').join('<br>');
+			// reformat for logs
 			text = text.replace(/\[(.*)\] (\S.{0,30}\S) : (.*)/g,'<span class="name">$2</span> <span class="time">$1</span><br>$3');
+			// custom emotes
+			text = text.replace(/&lt(:[A-Za-z0-9-_]{2,64}:)(\d{17,20})&gt/g,'<img src="https://cdn.discordapp.com/emojis/$2.png" alt="$1">');
 			$('#output').html('<a class="button" href="'+url+'">View Original</a><br><br>'+text);
+			twemoji.size = '16x16';
+			twemoji.parse(document.body);
 		});
 	else
 		$('#output').html('Invalid text file');
