06ea54df5b3fdca1d4b3295e6189a1ee947cae4b
John Grosh <john.a.grosh@gmail.com>
2017-12-16 21:16:52 -0500

    updated to account for ampersands and more spaces

--- js/custom.js
+++ js/custom.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
 	if(loc)
 		$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=?', function(data){
 			// filter html
-			var text = data.contents.split('<').join('&lt;').split('>').join('&gt;').split('\n').join('<br>');
+			var text = data.contents.split('&').join('&amp;').split('<').join('&lt;').split('>').join('&gt;').split('\n').join('<br>');
 			// reformat for logs (<md> tokens for markdown later)
 			text = text.replace(/\[(.*)\] (\S.{0,62}\S) : (.*)/g,'<md/><span class="name">$2</span> <span class="time">$1</span><br><md>$3');
 			// markdown
@@ -37,10 +37,10 @@ $(document).ready(function() {
 				else
 					return markdown(s);
 			}).join('');
-      // custom emotes
+			// custom emotes
 			text = text.replace(/&lt;(:[A-Za-z0-9-_]{2,64}:)(\d{17,20})&gt;/g,'<img src="https://cdn.discordapp.com/emojis/$2.png" class="emote" alt="$1">');
 			// update
-			$('#output').html('<a class="button" href="'+url+'">Download Original</a><br><br>'+text);
+			$('#output').html('<a class="button" href="'+url+'">Download Original</a><br><br>'+(text.replace(/  /g,' &nbsp;')));
 			twemoji.size = '16x16';
 			twemoji.parse(document.body);
 		});
