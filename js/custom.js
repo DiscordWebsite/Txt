5b9378fbeb100fe8832117b6427a631f1a5cc156
John Grosh <john.a.grosh@gmail.com>
2018-10-28 14:41:55 -0400

    https support and raw query param

--- js/custom.js
+++ js/custom.js
@@ -21,32 +21,43 @@ var markdown = function(source) {
 // Loading doc and parsing
 $(document).ready(function() {
 	var loc = getParameterByName('txt')
+    var raw = getParameterByName('raw')
 	var url = "https://cdn.discordapp.com/attachments/"+loc+".txt";
 	if(loc)
-		$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=?', function(data){
+		$.getJSON('https://allorigins.me/get?url=' + encodeURIComponent(url) + '&callback=?', function(data){
 			// filter html
 			var text = data.contents.split('&').join('&amp;').split('<').join('&lt;').split('>').join('&gt;').split('\n').join('<br>');
-			// reformat for logs (<md> tokens for markdown later)
-			// text = text.replace(/\[(.*)\] (\S.{0,70}\S) : (.*)/g,'<md/><span class="name">$2</span> <span class="time">$1</span><br><md>$3');
-            // changed this line to add support for user IDs
-            text = text.replace(/\[(.*)\] (\S.{0,70}?\S) (\(\d{17,20}\) )?: (.*)/g,'<md/><span class="name">$2</span> <span class="time">$3$1</span><br><md>$4');
-			// markdown
-			text = text.split('<md').map(s => {
-				if(s.startsWith('/>'))
-					return s.substring(2);
-			  else if(s.startsWith('>'))
-					return markdown(s.substring(1));
-				else
-					return markdown(s);
-			}).join('');
-			// custom emotes
-			text = text.replace(/&lt;(:[A-Za-z0-9-_]{2,64}:)(\d{17,20})&gt;/g,'<img src="https://cdn.discordapp.com/emojis/$2.png?v=1" class="emote" alt="$1">');
-			text = text.replace(/&lt;a(:[A-Za-z0-9-_]{2,64}:)(\d{17,20})&gt;/g,'<img src="https://cdn.discordapp.com/emojis/$2.gif?v=1" class="emote" alt="$1">');
-			// update
-			$('#output').html('<a class="button" href="'+url+'">Download Original</a><br><br>'+(text.replace(/  /g,' &nbsp;')));
-			twemoji.size = '16x16';
-			twemoji.parse(document.body);
+            if(!raw)
+            {
+                // reformat for logs (<md> tokens for markdown later)
+                // text = text.replace(/\[(.*)\] (\S.{0,70}\S) : (.*)/g,'<md/><span class="name">$2</span> <span class="time">$1</span><br><md>$3');
+                // changed this line to add support for user IDs
+                text = text.replace(/\[(.*)\] (\S.{0,70}?\S) (\(\d{17,20}\) )?: (.*)/g,'<md/><span class="name">$2</span> <span class="time">$3$1</span><br><md>$4');
+                // markdown
+                text = text.split('<md').map(s => {
+                    if(s.startsWith('/>'))
+                        return s.substring(2);
+                  else if(s.startsWith('>'))
+                        return markdown(s.substring(1));
+                    else
+                        return markdown(s);
+                }).join('');
+                // custom emotes
+                text = text.replace(/&lt;(:[A-Za-z0-9-_]{2,64}:)(\d{17,20})&gt;/g,'<img src="https://cdn.discordapp.com/emojis/$2.png?v=1" class="emote" alt="$1">');
+                text = text.replace(/&lt;a(:[A-Za-z0-9-_]{2,64}:)(\d{17,20})&gt;/g,'<img src="https://cdn.discordapp.com/emojis/$2.gif?v=1" class="emote" alt="$1">');
+            }
+            // update
+            $('#output').html('<a class="button" href="'+url+'">Download Original</a><br><br>'+(text.replace(/  /g,' &nbsp;')));
+            //twemoji
+            if(!raw)
+            {
+                twemoji.size = '16x16';
+                twemoji.parse(document.body);
+            }
 		});
 	else
-		$('#output').html('Invalid text file');
+		$('#output').html('No text file provided.<br><br>'
+    +'This site is used to view .txt files that have been uploaded to Discord.<br><br>'
+    +'For example, the file uploaded here: https://cdn.discordapp.com/attachments/147698382092238848/506154212124786689/example.txt<br><br>'
+    +'Can be viewed here: https://txt.discord.website/?txt=147698382092238848/506154212124786689/example<br><br>');
 });
